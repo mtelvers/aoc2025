46354cef2a980437b987633b5dcd0b8fb5a54308
Mark Elvers <mark.elvers@tunbury.org>
2025-12-02 11:27:33 +0000

    Day 2

---
+++ day2/README.md
@@ -0,0 +1,32 @@
+# Day 2 - Gift Shop
+
+Find repeating patterns in some number ranges.
+
+```
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
+```
+
+## Part 1
+
+I decided right away to use integer comparison rather than converting numbers to strings and then comparing them. The number of digits in an integer when written in base 10 is the `1 + int(log10 x)`. For part 1, the challenge was to look for exact splits `11` or `123123`; therefore, the length must be even. We can use the divisor `10^(length/2)`, and test with `x / divisor = x mod divisor` and sum all the numbers where this is true.
+
+## Part 2
+
+The problem is extended to allow any even chunking. Thus, `824824824` is now valid as it has three chunks of 3 digits. Given the maximum length of a 64-bit integer is 20 digits, we only need the factors of the numbers 1 to 20, which could be entered as a static list. I decided to calculate these in code using a simple division test up to the square root of the number. I should memoise these results to avoid repeated recalculation. Once I had a list of factors, I folded over the list, testing each with a recursive function to verify that each chunk was equal.
+
+```ocaml
+let base = pow 10 f in
+let modulo = x mod base in
+
+let rec loop v =
+  if v = 0 then true
+  else if v mod base = modulo then loop (v / base)
+  else false
+in
+
+loop (x / base)
+```
+
+The only gotcha I found was that numbers less than 10 came out as true, so I constrained the lower bound of the range to 10.
--- day2/main.ml
+++ day2/main.ml
@@ -4,7 +4,9 @@ let input =
 let pairs =
   String.concat "" input |> String.split_on_char ','
   |> List.map (fun pair ->
-         String.split_on_char '-' pair |> List.map int_of_string)
+         String.split_on_char '-' pair |> List.map int_of_string |> function
+         | a :: b :: _ -> (a, b)
+         | _ -> assert false)
 
 let is_odd x = x land 1 = 1
 
@@ -15,22 +17,42 @@ let rec pow a = function
       let b = pow a (n / 2) in
       b * b * if is_odd n then a else 1
 
-let check l h =
+let factors n =
+  let root = int_of_float (sqrt (float_of_int n)) in
+  let rec loop x =
+    if x > root then []
+    else if n mod x > 0 then loop (x + 1)
+    else if x = n / x || x = 1 then x :: loop (x + 1)
+    else x :: (n / x) :: loop (x + 1)
+  in
+  loop 1
+
+let factors2 n = if is_odd n then [] else [ n / 2 ]
+
+let check fn l h =
   let rec loop x =
     if x > h then 0
     else
       let len = 1 + int_of_float (log10 (float_of_int x)) in
-      if is_odd len then loop (x + 1)
-      else
-        let base = pow 10 (len / 2) in
-        if x / base = x mod base then x + loop (x + 1) else loop (x + 1)
+      let fl = fn len in
+      List.fold_left
+        (fun acc f ->
+          let base = pow 10 f in
+          let modulo = x mod base in
+          let rec loop2 v =
+            if v = 0 then true
+            else if v mod base = modulo then loop2 (v / base)
+            else false
+          in
+          acc || loop2 (x / base))
+        false fl
+      |> function
+      | false -> loop (x + 1)
+      | true -> x + loop (x + 1)
   in
-  loop l
+  loop (max l 10)
 
-let part1 =
-  List.fold_left
-    (fun sum pair ->
-      match pair with l :: h :: _ -> sum + check l h | _ -> assert false)
-    0 pairs
-
+let part1 = List.fold_left (fun sum (l, h) -> sum + check factors2 l h) 0 pairs
 let () = Printf.printf "part 1: %i\n" part1
+let part2 = List.fold_left (fun sum (l, h) -> sum + check factors l h) 0 pairs
+let () = Printf.printf "part 2: %i\n" part2
